b849c44a0b20011b5290fda9e9e8f8c1e48106b8
Jonathan Herman <jdh11235@gmail.com>
2015-01-22 23:41:01 -0600

    added canelLoops function

--- generator.js
+++ generator.js
@@ -5,16 +5,24 @@ Array.prototype.random = function () {
 
 var Music = {
 
+	cancelLoops: function() {
+		while (Music.loops.length > 0) {
+			clearInterval(Music.loops.pop());
+		}
+	},
+
+	//speed is in seconds
+	loopRandom: function(instrument, octave, speed) {
+		Music.loops.push(setInterval(function() {
+			Music.playRandom(instrument, octave, speed);
+		}, speed * 1000));
+	},
+
+	loops: [],
+
 	playRandom: function(instrument, octave, speed) {
 		var note = ['A', 'B', 'C', 'D', 'E', 'F', 'G'].random();
 		Synth.play(instrument, note, octave, speed);
-	},
-
-	loopRandom: function(instrument, octave, speed) {
-	//speed is in seconds
-		setInterval(function() {
-			Music.playRandom(instrument, octave, speed);
-		}, speed * 1000);
 	}
 
 };
